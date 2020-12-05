9d5e5ade8172c9895c487d41d4f4858e95a692a2
Bob <bobisageek@gmail.com>
2020-12-05 11:44:01 -0500

    add day 4

--- src/main/kotlin/io/github/bobisageek/aoc2020kt/utils.kt
+++ src/main/kotlin/io/github/bobisageek/aoc2020kt/utils.kt
@@ -1,6 +1,8 @@
 package io.github.bobisageek.aoc2020kt
 
-fun dayLines(fileName: String) = "$fileName.txt".let { ClassLoader.getSystemResource(it) }
-    .readText().lines().dropLastWhile { it.isBlank() }
+fun dayText(fileName: String) = "$fileName.txt".let { ClassLoader.getSystemResource(it) }
+    .readText()
+
+fun dayLines(fileName: String) = dayText(fileName).lines().dropLastWhile { it.isBlank() }
 
 fun Iterable<CharSequence>.regexExtract(rgx: Regex) = this.map { rgx.matchEntire(it)?.groupValues?.drop(1) }
