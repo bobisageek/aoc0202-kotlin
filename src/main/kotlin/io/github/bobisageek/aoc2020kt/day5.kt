08abc4a00d2d29dc089445870f11929d26e55e93
Bob <bobisageek@gmail.com>
2020-12-05 16:03:39 -0500

    add day 5

--- src/main/kotlin/io/github/bobisageek/aoc2020kt/day5.kt
+++ src/main/kotlin/io/github/bobisageek/aoc2020kt/day5.kt
@@ -1,5 +1,18 @@
 package io.github.bobisageek.aoc2020kt
 
-fun main() {
+import java.util.*
 
+fun main() {
+    val seatNumberSet = dayLines("5").map(Day5::seatNum).toSortedSet()
+    println(Day5.part1(seatNumberSet))
+    println(Day5.part2(seatNumberSet))
+}
+
+object Day5 {
+    fun seatNum(seatCode: String) = seatCode.map {
+        mapOf('B' to 1, 'R' to 1).getOrDefault(it, 0)
+    }.joinToString(separator = "").let { Integer.parseInt(it, 2) }
+
+    fun part1(input: SortedSet<Int>) = input.last()
+    fun part2(input: SortedSet<Int>) = input.zip(input.drop(1)).first { (a, b) -> b - a != 1 }.let { it.first + 1 }
 }
